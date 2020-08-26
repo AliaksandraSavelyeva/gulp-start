7b23cace411f4632a3fc0a7e07e746db25469216
Savelyeva_A <Aliaksandra.Savelyeva@ibm.com>
2020-08-27 00:18:15 +0300

    browser-sync plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
 let gulp = require('gulp'),        //переменная для галпа
-    sass = require('gulp-sass');   //переменная для плагина перевода scss в css
+    sass = require('gulp-sass'),   //плагин перевода scss в css
+    browserSync = require('browser-sync');   //плагин browser-sync
 
 gulp.task('scss', function() {              //таск для перевода scss в css с использованием плагина
     return gulp.src('app/scss/**/*.scss')   //взять все файлы с разрешением .scss в директории
         .pipe(sass({outputStyle: 'expanded'}))   //опции compressed/expanded - выгрузить в сжатом/приличном виде
         .pipe(gulp.dest('app/css'))         //выгрузить результат в указанную папку
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('html', function() {          //таск для отслеживания browser-sync плагином html-файлов
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {      //плагин для live reload
+    browserSync.init({
+        server: { baseDir: 'app/' }
+    })
 });
 
 gulp.task('watch', function() {
-    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))     //наблюдатель, при изменении
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));     //наблюдатель, при изменении scss файлов
+    gulp.watch('app/*.html', gulp.parallel('html'))     //наблюдатель, при изменении html файлов
 });
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));     //параллельные таски без заморозки консоли
