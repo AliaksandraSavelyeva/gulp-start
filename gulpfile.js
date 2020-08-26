55987a4ca476d7630a4c402c6009dd120dc87e96
Savelyeva_A <Aliaksandra.Savelyeva@ibm.com>
2020-08-27 00:42:58 +0300

    task that concatenates and minifies js plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 let gulp = require('gulp'),        //переменная для галпа
     sass = require('gulp-sass'),   //плагин перевода scss в css
-    browserSync = require('browser-sync');   //плагин browser-sync
+    browserSync = require('browser-sync'),   //плагин browser-sync
+    uglify = require('gulp-uglify'),    
+    concat = require('gulp-concat');
 
 gulp.task('scss', function() {              //таск для перевода scss в css с использованием плагина
     return gulp.src('app/scss/**/*.scss')   //взять все файлы с разрешением .scss в директории
@@ -14,6 +16,17 @@ gulp.task('html', function() {          //таск для отслеживани
         .pipe(browserSync.reload({stream: true}))
 });
 
+gulp.task('js', function() {        //таск для объединения всех js-плагинов в один сжатый файл
+    return gulp.src([                                    // [] - в директории несколько файлов
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+    ])
+        .pipe(concat('libs.min.js'))        //объединение
+        .pipe(uglify())                          //минифицирование
+        .pipe(gulp.dest('app/js'))
+        .pipe(browserSync.reload({stream: true}))       //отслеживание js-изменений
+});
+
 gulp.task('browser-sync', function() {      //плагин для live reload
     browserSync.init({
         server: { baseDir: 'app/' }
@@ -22,7 +35,8 @@ gulp.task('browser-sync', function() {      //плагин для live reload
 
 gulp.task('watch', function() {
     gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));     //наблюдатель, при изменении scss файлов
-    gulp.watch('app/*.html', gulp.parallel('html'))     //наблюдатель, при изменении html файлов
+    gulp.watch('app/*.html', gulp.parallel('html'));     //наблюдатель, при изменении html файлов
+    gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
 
 gulp.task('default', gulp.parallel('browser-sync', 'watch'));     //параллельные таски без заморозки консоли
